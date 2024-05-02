1b0ab9bd837c344570b6287ca4ac102566953991
Rhuan-Gonzaga <rhuangonzaga22@gmail.com>
2024-05-02 07:04:42 -0300

    Erro na requisicao na hora de editar cliente

--- routes/cliente.py
+++ routes/cliente.py
@@ -35,12 +35,26 @@ def detalhe_cliente(cliente_id):
 #Formulario para editar um cliente
 @cliente_route.route('/<int:cliente_id>/edit')
 def form_edit_cliente(cliente_id):
-    return render_template('form_edit_cliente.html')
+    cliente = None
+    for c in CLIENTES:
+        if c['id'] == cliente_id:
+            cliente = c
+    return render_template('form_cliente.html',cliente=cliente)
 
 #Formulario para atualizar  um cliente
 @cliente_route.route('/<int:cliente_id>/update',methods=['PUT'])
 def atualizar_cliente(cliente_id):
-    pass
+    cliente_editado = None
+    data = request.json
+    for c in CLIENTES:
+        if c['id'] == cliente_id:
+            c['nome'] = data['nome']
+            c['email'] = data['email']
+
+            cliente_editado = c
+    
+    return render_template('item_cliente.html',cliente= cliente_editado)
+
 
 #Formulario para deletar  um cliente
 @cliente_route.route('/<int:cliente_id>/delete',methods=['DELETE'])

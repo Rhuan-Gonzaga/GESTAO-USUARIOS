a6ce75894e1f0367514369c049166810d05b1f69
Rhuan-Gonzaga <rhuangonzaga22@gmail.com>
2024-05-11 10:11:40 -0300

    Mudando de dict para um bando SQLite e usando a ORM Peewee

--- routes/cliente.py
+++ routes/cliente.py
@@ -1,24 +1,22 @@
 from flask import Blueprint, render_template, request
-from database.cliente import CLIENTES
+from database.models.cliente import Cliente
 
 cliente_route = Blueprint('cliente',__name__)
 
 #Listar os clientes
 @cliente_route.route('/')
 def lista_clientes():
-    return render_template('lista_clientes.html',clientes=CLIENTES)
+    clientes = Cliente.select()
+    return render_template('lista_clientes.html',clientes=clientes)
 
 #Inserir os dados do cliente
 @cliente_route.route('/',methods=['POST'])
 def inserir_cliente():
     data = request.json
-    novo_usuario = {
-        "id": len(CLIENTES) + 1,
-        "nome": data['nome'],
-        "email": data['email']
-    }
-
-    CLIENTES.append(novo_usuario)
+    novo_usuario = Cliente.create(
+        nome = data['nome'],
+        email = data['email']
+    )
     return render_template('item_cliente.html',cliente=novo_usuario)
 
 #Formulario para cadastrar um cliente
@@ -29,37 +27,30 @@ def form_cliente():
 #Exibir detalhes do cliente
 @cliente_route.route('/<int:cliente_id>')
 def detalhe_cliente(cliente_id):
-    cliente = list(filter(lambda c: c['id'] == cliente_id,CLIENTES))[0]
+    cliente = Cliente.get(Cliente.id == cliente_id)
     return render_template('detalhe_cliente.html',cliente=cliente)
 
 
 #Formulario para editar um cliente
 @cliente_route.route('/<int:cliente_id>/edit')
 def form_edit_cliente(cliente_id):
-    cliente = None
-    for c in CLIENTES:
-        if c['id'] == cliente_id:
-            cliente = c
+    cliente = Cliente.get_by_id(cliente_id)
     return render_template('form_cliente.html',cliente=cliente)
 
 #Formulario para atualizar  um cliente
 @cliente_route.route('/<int:cliente_id>/update',methods=['PUT'])
 def atualizar_cliente(cliente_id):
-    cliente_editado = None
     data = request.json
-    for c in CLIENTES:
-        if c['id'] == cliente_id:
-            c['nome'] = data['nome']
-            c['email'] = data['email']
-
-            cliente_editado = c
-    
+    cliente_editado = Cliente.get_by_id(cliente_id)
+    cliente_editado.nome = data['nome']
+    cliente_editado.email = data['email']
+    cliente_editado.save()
     return render_template('item_cliente.html',cliente= cliente_editado)
 
 
 #Formulario para deletar  um cliente
 @cliente_route.route('/<int:cliente_id>/delete',methods=['DELETE'])
 def deletar_cliente(cliente_id):
-    global CLIENTES
-    CLIENTES = [ c for c in CLIENTES if c['id'] != cliente_id]
+    cliente = Cliente.get_by_id(cliente_id)
+    cliente.delete_instance()
     return {'deleted': 'ok'}
